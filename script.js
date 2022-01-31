a2a25cf5c45cfc3f4fc623f6e4f1b340c71be353
Ustin.Vaskin <51622314+ustinvaskin@users.noreply.github.com>
2022-01-31 23:25:10 +0100

    update js w comments

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const searchInput = document.querySelector("[data-search]")
 let users = []
 
 searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase() // getting a value from input
-  users.forEach(user => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => { // looping through users
     const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
+      user.name.toLowerCase().includes(value) || // checking name form input 
+      user.email.toLowerCase().includes(value) // checking email form input 
+    user.element.classList.toggle("hide", !isVisible) // using class to hide/ show 
   })
 })
 
@@ -18,12 +18,12 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0] // getting first elem of a fragment
+      const card = userCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
       header.textContent = user.name
       body.textContent = user.email
-      userCardContainer.append(card) // appending
-      return { name: user.name, email: user.email, element: card }
+      userCardContainer.append(card)
+      return { name: user.name, email: user.email, element: card } // returning only these keys
     })
   })
